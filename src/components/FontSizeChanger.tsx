1dc026c30d03b29438d2796caf74ac9a84ac5f28
Krzysiek-U <krzysiek@galeria.net.pl>
2022-09-30 12:21:45 +0200

    tabIndex

--- src/components/FontSizeChanger.tsx
+++ src/components/FontSizeChanger.tsx
@@ -16,9 +16,9 @@ const FontSizeChanger = () => {
 
     return (
       <span id='changeFontSizeContener'>
-        <a id='changeFontSizeDown' title='Zmniejsz wielkość czcionki' className='a-button font-size-change' role="button" onClick={ () =>  htmlFont(rootFontSize-2)  }> - </a>
-        <a id='changeFontSizeStart' title='Ustaw standardową wielkść czcionki' className='a-button font-size-change' role="button" onClick={ () =>  htmlFont(rootFontSizeStart)  }> A </a>
-        <a id='changeFontSizeUp' title='Zwiększ wielkość czcionki' className='a-button font-size-change' role="button" onClick={ () =>  htmlFont(rootFontSize+2)  }> + </a>
+        <a id='changeFontSizeDown' title='Zmniejsz wielkość czcionki' className='a-button font-size-change' role="button" tabIndex={0} onClick={ () =>  htmlFont(rootFontSize-2)  }> - </a>
+        <a id='changeFontSizeStart' title='Ustaw standardową wielkść czcionki' className='a-button font-size-change' role="button" tabIndex={0} onClick={ () =>  htmlFont(rootFontSizeStart)  }> A </a>
+        <a id='changeFontSizeUp' title='Zwiększ wielkość czcionki' className='a-button font-size-change'  role="button" tabIndex={0} onClick={ () =>  htmlFont(rootFontSize+2)  }> + </a>
       </span>
   );
 };
--- src/components/NextPrev.tsx
+++ src/components/NextPrev.tsx
@@ -10,9 +10,9 @@ const NextPrev = ( props: NextPrevProps ) => {
     return (
       <div id='nextPrevtContener' className='next-prev contrast'>
         <div className='next-prev-container'>
-          <a id='prevPhotos' title='Poprzednie zdjęcia' className='a-button prev' role="button" onClick={ () =>  props.changePage('prev') }><i className="fa fa-chevron-left"></i> Poprzednie</a>
+          <a id='prevPhotos' title='Poprzednie zdjęcia' className='a-button prev' role="button" tabIndex={0} onClick={ () =>  props.changePage('prev') }><i className="fa fa-chevron-left"></i> Poprzednie</a>
           <span>{props.page}/ {props.maxPhotoNumb/props.limit }</span>
-          <a id='nextPhotos' title='Następne zdjęcia' className='a-button next' role="button" onClick={ () =>  props.changePage('next') }>Następne <i className="fa fa-chevron-right"></i></a>
+          <a id='nextPhotos' title='Następne zdjęcia' className='a-button next' role="button" tabIndex={0} onClick={ () =>  props.changePage('next') }>Następne <i className="fa fa-chevron-right"></i></a>
         </div>
       </div>
   );

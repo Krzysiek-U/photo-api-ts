31397acd9e32a8bdac586ce941bee6fd19992d62
Krzysiek-U <krzysiek@galeria.net.pl>
2022-10-02 19:39:18 +0200

    HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React  from 'react';
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-//import { HashRouter as Router, Link, Route, Routes  } from 'react-router-dom'
+//import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Link, Route, Routes  } from 'react-router-dom'
 import Nav from './components/Nav';
 import Main from './components/Main';
 import PhotoBig from './components/PhotoBig';
@@ -20,6 +20,7 @@ function App() {
           <Route path= '/' element={<Main/>} />
           <Route path= '/photo' element={<PhotoBig/>} />
           <Route path= '/about' element={<About/>} />
+          <Route path= '/photo-api-ts/about' element={<About/>} />
         </Routes>
         <footer className="footer contrast">
           <Footer/>

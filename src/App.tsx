7f1c29afc4b1c5fc18e61ae70b6917ae1ae6f27d
Krzysiek-U <krzysiek@galeria.net.pl>
2022-10-02 18:35:08 +0200

    path= '/photo-api-ts/'

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,16 @@ import './App.css';
 function App() {
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>   
+    //<Router basename={process.env.PUBLIC_URL}>   
+    <Router >   
       <div className="App contrast" >
         <header className="header contrast">
           <Nav/>
         </header>        
         <Routes>
-          <Route path= '/' element={<Main/>} />
-          <Route path= '/photo' element={<PhotoBig/>} />
-          <Route path= '/about' element={<About/>} />
+          <Route path= '/photo-api-ts/' element={<Main/>} />
+          <Route path= '/photo-api-ts/photo' element={<PhotoBig/>} />
+          <Route path= '/photo-api-ts/about' element={<About/>} />
         </Routes>
         <footer className="footer contrast">
           <Footer/>

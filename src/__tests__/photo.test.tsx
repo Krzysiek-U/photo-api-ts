8fb40bfc2777501237af9e23f25685a7cdb906d2
Krzysiek-U <krzysiek@galeria.net.pl>
2022-10-08 23:52:43 +0200

    tests photo

--- src/__tests__/photo.test.tsx
+++ src/__tests__/photo.test.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { BrowserRouter as Router} from "react-router-dom";
 import { render } from '@testing-library/react';
 import Photo from '../components/Photo';
+//import { createMemoryHistory } from 'history';
+
 
 const photo = { id : 1, author : 'Alejandro Escamilla' };
 const index = 1;
 
+
 describe( 'Photo component', () => {
     test('should match the snapshot', () => {
-        const { container } = render(<Router>  <Photo key={index} photo={photo} /> </Router>  );
+        //const history = createMemoryHistory({ initialEntries: ['/'] });
+        //const history = createMemoryHistory({ initialEntries: ['/page1', '/page2'] });
+        const { container } = render(
+            <Router >
+                <Photo photo={photo} />
+            </Router>  
+        );
         expect(container).toMatchSnapshot();
     })    
 });
